70a89bb53e92608c47993c56ea4f97da34a9f630
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-08-28 17:06:42 -0300

    Update registra.R

--- registra.R
+++ registra.R
@@ -3,3 +3,30 @@ library(devtools)
 
 source("executa.R")
 
+salvar <- function(i){
+  arquivo<-paste0(sprintf("%02d",i), ".RData")
+  resultados <- resultados[[i]]
+  distribution <- planos[[i]]$distribution
+  obs <- planos[[i]]$obs
+  caso_normal <- function(x){
+    sigma2 <- x$sigma2
+    mu <- x$mu
+    save(sigma2,mu,obs,distribution,resultados, file= arquivo)
+  }
+  caso_poisson <- function(x){
+    lambda <- x$lambda
+    save(lambda,obs,distribution,resultados, file= arquivo)
+  }
+  caso_binomial <- function(x){
+    p <- x$p
+    save(p,obs,distribution,resultados, file= arquivo)
+  }
+  switch (planos[[i]]$distribution,
+    "normal" = caso_normal(planos[[i]]),
+    "poisson" = caso_poisson(planos[[i]]),
+    "bernoulli" = caso_binomial(planos[[i]])
+  )
+}
+
+walk(seq_along(planos),\(i) salvar(i))
+
