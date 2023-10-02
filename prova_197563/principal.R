af3bc00a9ae6cb4754358fbeb6b7bc8642f57f8b
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-10-02 17:24:33 -0300

    Create principal.R

---
+++ prova_197563/principal.R
@@ -0,0 +1,9 @@
+library(yaml)
+
+#Caso 1
+library(revisao199910)
+
+#Caso 2
+#Dado que o nosso arquivo tar.gz já está na pasta do projeto
+devtools::install_local("revisao199910_0.1.0.tar.gz")
+library(revisao199910)
