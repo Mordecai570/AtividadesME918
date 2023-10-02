a91f990dba471617ea7a7f7a552f81c82b7a7836
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-10-02 17:28:55 -0300

    Update principal.R

--- prova_197563/principal.R
+++ prova_197563/principal.R
@@ -7,3 +7,17 @@ library(revisao199910)
 #Dado que o nosso arquivo tar.gz já está na pasta do projeto
 devtools::install_local("revisao199910_0.1.0.tar.gz")
 library(revisao199910)
+
+
+# Pré-requisitos Probabilidade II
+leitura$cursos$`estatística`$disciplinas[[2]]$`pre-requisitos`
+
+library(jsonlite)
+
+# Leitura arquivo json banco Penguins
+jsonlite::read_json("penguins.json", simplifyDataFrame = TRUE) -> df_penguins
+tibble::tibble(df_penguins)
+
+# Leitura arquivo json banco Diamonds
+jsonlite::read_json("penguins.json", simplifyDataFrame = TRUE) -> df_diamonds
+tibble::tibble(df_diamonds)
