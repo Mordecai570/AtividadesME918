1471c4553f58e00e1354b0c5c1588370f84ccfc1
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-10-02 21:19:17 -0300

    Update principal.R

--- prova_197563/principal.R
+++ prova_197563/principal.R
@@ -3,9 +3,5 @@ library(prova197563)
 library(jsonlite)
 
 # Leitura arquivo json banco Penguins
-jsonlite::read_json("penguins.json", simplifyDataFrame = TRUE) -> df_penguins
-tibble::tibble(df_penguins)
-
-# Leitura arquivo json banco Diamonds
-jsonlite::read_json("diamonds.json", simplifyDataFrame = TRUE) -> df_diamonds
-tibble::tibble(df_diamonds)
+jsonlite::read_json("penguins.json", simplifyDataFrame = TRUE) -> df
+tibble::tibble(df)
