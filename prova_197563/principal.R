b3e1364b207bcf5323a094dbbac73bd895098ca0
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-10-02 21:34:51 -0300

    Update principal.R

--- prova_197563/principal.R
+++ prova_197563/principal.R
@@ -3,5 +3,5 @@ library(prova197563)
 library(jsonlite)
 
 # Leitura arquivo json banco Penguins
-jsonlite::read_json("penguins.json", simplifyDataFrame = TRUE) -> df
+jsonlite::read_json("~/Utilizando pacote da prova/data/penguins.json", simplifyDataFrame = TRUE) -> df
 tibble::tibble(df)
