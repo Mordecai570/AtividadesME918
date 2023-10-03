99f93fcbb3621bf1b269593766148b19dcdb7fa3
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-10-02 21:16:55 -0300

    Update principal.R

--- prova_197563/principal.R
+++ prova_197563/principal.R
@@ -1,17 +1,5 @@
 library(yaml)
-
-#Caso 1
 library(prova197563)
-
-#Caso 2
-#Dado que o nosso arquivo tar.gz já está na pasta do projeto
-devtools::install_local("prova197563_0.1.0.tar.gz")
-library(prova197563)
-
-
-# Pré-requisitos Probabilidade II
-leitura$cursos$`estatística`$disciplinas[[2]]$`pre-requisitos`
-
 library(jsonlite)
 
 # Leitura arquivo json banco Penguins
