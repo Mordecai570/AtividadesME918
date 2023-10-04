59d434a64c17734260c7d0b28c1341df894ac3e2
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-10-04 14:07:08 -0300

    Delete prova_197563/principal.R

--- prova_197563/principal.R
+++
@@ -1,7 +0,0 @@
-library(yaml)
-library(prova197563)
-library(jsonlite)
-
-# Leitura arquivo json banco Penguins
-jsonlite::read_json("~/Utilizando pacote da prova/data/penguins.json", simplifyDataFrame = TRUE) -> df
-tibble::tibble(df)
