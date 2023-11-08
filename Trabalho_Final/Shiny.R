814d1dd6c5e6dc30ed48d14965df73aa2be03918
GabrielVSS <73541458+Mordecai570@users.noreply.github.com>
2023-11-07 23:37:30 -0300

    Update Shiny.R

--- Trabalho_Final/Shiny.R
+++ Trabalho_Final/Shiny.R
@@ -1,16 +1,20 @@
 library(ggplot2)
 library(plotly)
+library(RCurl)
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
 library(shinyWidgets)
 
+df <- getURL("https://raw.githubusercontent.com/Mordecai570/AtividadesME918/main/Trabalho_Final/cereal.csv")
 
-ui <-  navbarPage(selected = "precos", #theme = shinytheme("cerulean"),
+df <- read.csv(text = df)
+
+ui <-  navbarPage(selected = "df", theme = shinytheme("cerulean"),
                   "Projeto",
                   tabPanel("Primeira aba"),
                   tabPanel("Modelo", numericInput("x","caloria", value = 0 ), numericInput("y","gordura", value = 0),numericInput("z","proteina", value = 0 )),
-                  tabPanel("Pesquisa", selectInput("Cereal", "Cereal favorito?",state.name)),
+                  tabPanel("Pesquisa", selectInput(inputId = "Cereal", label = "Cereal favorito?",choices = df$name, selected = "All",multiple = TRUE),),
                   tabPanel("Cadastro"),
                   tabPanel("EAD" )
                            
@@ -19,7 +23,7 @@ ui <-  navbarPage(selected = "precos", #theme = shinytheme("cerulean"),
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
 }
 
